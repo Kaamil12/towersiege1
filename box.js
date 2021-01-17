1c98a17549726822abb12fd6c14fcbc75189848b
Kaamil12 <69597401+Kaamil12@users.noreply.github.com>
2021-01-17 14:14:10 -0600

    Add files via upload

--- box.js
+++ box.js
@@ -5,3 +5,22 @@ class Box extends BaseClass {
     }
   
   };
+  function score(){
+
+    block1.score();
+    block2.score();
+    block3.score();
+    block4.score();
+    block5.score();
+    block6.score();
+    block7.score();
+    block8.score();
+    block9.score();
+    block10.score();
+    block11.score();
+    block12.score();
+    block13.score();
+    block14.score();
+    block15.score();
+    block16.score();
+  }
